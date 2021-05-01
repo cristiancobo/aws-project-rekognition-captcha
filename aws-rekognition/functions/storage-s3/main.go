f49d364f292c9db609540d8d05e7bb1141a94efb
Joe <jhernandez@truora.com>
2021-05-01 18:10:24 -0500

    Functions to upload s3 was add

--- aws-rekognition/functions/storage-s3/main.go
+++ aws-rekognition/functions/storage-s3/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/base64"
+	"fmt"
+	"net/http"
 	"net/url"
 	"os"
 	"time"
@@ -9,10 +13,44 @@ import (
 	"github.com/aws-project-rekognition-captcha/shared/apigateway"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
+	"github.com/aws/aws-xray-sdk-go/xray"
 )
 
 var (
-	awsS3ARN = GetString("AWS_S3_ARN", "arn:aws:s3:::estudiantes-cloud-2021-3")
+	// Client is the client for the S3 service
+	Client s3iface.S3API
+
+	bucket       = GetString("IMAGES_BUCKET", "estudiantes-cloud-2021-3")
+	mapImgFormat = map[string]format{
+		"/9j/": {
+			Format:      ".jpg",
+			ContentType: "image/jpeg",
+		},
+		"iVBO": {
+			Format:      ".png",
+			ContentType: "image/png",
+		},
+		"UE5H": {
+			Format:      ".png",
+			ContentType: "image/png",
+		},
+		"R0lG": {
+			Format:      ".gif",
+			ContentType: "image/png",
+		},
+		"Qk0u": {
+			Format:      ".bmp",
+			ContentType: "image/bmp",
+		},
+		"Qk1W": {
+			Format:      ".bmp",
+			ContentType: "image/bmp",
+		},
+	}
 )
 
 // GetString gets the env var as a string
@@ -35,9 +73,51 @@ type Storage struct {
 	image string
 	name  string
 }
+type format struct {
+	Format      string
+	ContentType string
+}
+
+type resposeAPI struct {
+	Name   string `json:"Name"`
+	Path   string `json:"Path"`
+	Bucket string `json:"Bucket"`
+}
+
+func init() {
+	client := s3.New(session.Must(session.NewSession()))
+	xray.AWS(client.Client)
+
+	Client = client
+}
+
+func uploadObject(ctx context.Context, bucket, path, contentType string, data []byte, metadata map[string]string) error {
+	input := &s3.PutObjectInput{
+		Body:        bytes.NewReader(data),
+		Bucket:      aws.String(bucket),
+		Key:         aws.String(path),
+		Metadata:    aws.StringMap(metadata),
+		ContentType: aws.String(contentType),
+	}
+
+	_, err := Client.PutObjectWithContext(ctx, input)
+	return err
+}
+
+func getFormat(encodedBase64 string) (string, string) {
+	key := encodedBase64[:4]
+
+	format, ok := mapImgFormat[key]
+	if ok {
+		return format.Format, format.ContentType
+	}
+
+	return "", ""
+}
 
 func apiGatewayHandler(ctx context.Context, request *Request) (*apigateway.Response, error) {
 	queryParams, err := url.ParseQuery(request.Body)
+	fmt.Printf("Body %s", request.Body)
 	if err != nil {
 		return apigateway.NewErrorResponse(apigateway.ErrInvalidRequest), nil
 	}
@@ -46,10 +126,32 @@ func apiGatewayHandler(ctx context.Context, request *Request) (*apigateway.Respo
 
 	storage.image = queryParams.Get("image")
 	storage.name = queryParams.Get("name")
+	fmt.Printf("image %s", storage.image)
+	fmt.Printf("name %s", storage.name)
 
-	// TO DO, save in S3.
+	format, contentType := getFormat(storage.image)
+	fmt.Printf("format %s", format)
+	fmt.Printf("content type %s", contentType)
 
-	return nil, nil
+	path := storage.name + format
+
+	decodedImage, err := base64.StdEncoding.DecodeString(storage.image)
+	if err != nil {
+		return apigateway.NewErrorResponse(err), nil
+	}
+
+	err = uploadObject(ctx, bucket, path, contentType, decodedImage, nil)
+	if err != nil {
+		return apigateway.NewErrorResponse(err), nil
+	}
+
+	resposeAPI := resposeAPI{
+		Name:   storage.name,
+		Path:   path,
+		Bucket: bucket,
+	}
+
+	return apigateway.NewJSONResponse(http.StatusAccepted, resposeAPI), nil
 }
 
 func main() {
--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.16
 
 require (
 	github.com/aws/aws-lambda-go v1.23.0
+	github.com/aws/aws-sdk-go v1.38.30
+	github.com/aws/aws-xray-sdk-go v1.3.0
 	github.com/stretchr/testify v1.6.1
 )
